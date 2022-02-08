fb6569759b60adf4723d58c82d30be4409958c55
Sashanikitenkova <81653543+Sashanikitenkova@users.noreply.github.com>
2022-02-08 16:54:40 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+let promptCounter = 0;
+
+
+startBtn.addEventListener("click", () => {
+        timerId = setInterval(() => {
+        document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        promptCounter +=1;
+
+        if(promptCounter === 1) { 
+            startBtn.disabled = true;
+            promptCounter = 0; }
+        }, 1000);
+        
+      });
+  
+  
+  stopBtn.addEventListener("click", () => {
+        clearInterval(timerId);
+        startBtn.disabled = false;
+  });
+
+
 
