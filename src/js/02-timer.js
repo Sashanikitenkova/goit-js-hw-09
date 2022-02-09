742d357f5bd67a1c454d30687dc51f9bb6aaa2c2
Sashanikitenkova <81653543+Sashanikitenkova@users.noreply.github.com>
2022-02-09 14:07:01 +0200

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,12 +1,38 @@
-import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
+// import flatpickr from "flatpickr";
+// // Дополнительный импорт стилей
+// import 'flatpickr/dist/flatpickr.min.css';
 
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    },
-  };
+// const inputEl = document.querySelector("#datetime-picker");
+
+// const options = {
+  //     enableTime: true,
+  //     time_24hr: true,
+  //     defaultDate: new Date(),
+  //     minuteIncrement: 1,
+  //     onClose(selectedDates) {
+  //     console.log(selectedDates[0]);
+  //     },
+  //   };
+  
+// flatpickr(inputEl, options); 
+  
+ 
+
+  // function convertMs(ms) {
+  //   // Number of milliseconds per unit of time
+  //   const second = 1000;
+  //   const minute = second * 60;
+  //   const hour = minute * 60;
+  //   const day = hour * 24;
+  
+  //   // Remaining days
+  //   const days = Math.floor(ms / day);
+  //   // Remaining hours
+  //   const hours = Math.floor((ms % day) / hour);
+  //   // Remaining minutes
+  //   const minutes = Math.floor(((ms % day) % hour) / minute);
+  //   // Remaining seconds
+  //   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+  //   return { days, hours, minutes, seconds };
+  // }
