7ac98257a98e5de40d2d2954aef33b283e15307a
Sashanikitenkova <81653543+Sashanikitenkova@users.noreply.github.com>
2022-02-10 16:22:27 +0200

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,13 +1,21 @@
 import flatpickr from "flatpickr";
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import '../css/common.css';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Console } from "console";
 
 const inputEl = document.querySelector("#datetime-picker");
 const startBtn = document.querySelector('button[data-start]');
 
+const spanDays = document.querySelector('span[data-days]');
+const spanHours = document.querySelector('span[data-hours]');
+const spanMinutes = document.querySelector('span[data-minutes]');
+const spanSeconds = document.querySelector('span[data-seconds]');
+
+let intervalId = null;
 startBtn.disabled = true;
+
 
 const options = {
       enableTime: true,
@@ -15,36 +23,66 @@ const options = {
       defaultDate: new Date(),
       minuteIncrement: 1,
       onClose(selectedDates) {
-      console.log(selectedDates[0]);
+        if (selectedDates[0] > new Date()) {
+           startBtn.disabled = false;
+        } else {
+           startBtn.disabled = true;
+           Notify.failure("Please choose a date in the future");
+        }
       },
     };
   
-flatpickr(inputEl, options); 
+  let fp = flatpickr(inputEl, options); 
+
+  startBtn.addEventListener("click", () => {
+           intervalId = setInterval(() => {
+                     const ms = fp.selectedDates[0].getTime() - new Date().getTime();
+                     console.log(ms);
+                     const time = convertMs(ms);
+                     updateClockface(time);
+
+          }, 1000);
+
+          if ((fp.selectedDates[0].getTime() - new Date().getTime()) == 0) {
+            clearInterval(intervalId);
+          };
+         });
+
+  function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  };
+
+  function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  
+    return { days, hours, minutes, seconds };
+  };
+
+  function updateClockface ({ days, hours, minutes, seconds }) {
+    spanDays.textContent = `${days}`
+    spanHours.textContent = `${hours}`
+    spanMinutes.textContent = `${minutes}`
+    spanSeconds.textContent = `${seconds}`
+  };
+
   
 
-startBtn.addEventListener("click", () => {
-        // if() { 
-        //     startBtn.disabled = false;
-        //      }
-      });
-  
 
 
-  // function convertMs(ms) {
-  //   // Number of milliseconds per unit of time
-  //   const second = 1000;
-  //   const minute = second * 60;
-  //   const hour = minute * 60;
-  //   const day = hour * 24;
+
+
   
-  //   // Remaining days
-  //   const days = Math.floor(ms / day);
-  //   // Remaining hours
-  //   const hours = Math.floor((ms % day) / hour);
-  //   // Remaining minutes
-  //   const minutes = Math.floor(((ms % day) % hour) / minute);
-  //   // Remaining seconds
-  //   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   
-  //   return { days, hours, minutes, seconds };
-  // }
